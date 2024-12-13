e61d715d7a2e4f6a6786e40eb656bc540c20cd7a
jojohve <joseph.laluna@outlook.it>
2024-12-13 18:50:54 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./app/**/*.{vue,js,ts,jsx,tsx}"  // Aggiungi questa riga se i tuoi file sono nella directory app
   ],
   theme: {
     extend: {},
