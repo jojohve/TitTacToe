5049195e4ffc2fe2b5628f6ae9826a0d93caa76b
jojohve <joseph.laluna@outlook.it>
2024-12-13 17:07:24 +0100

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,7 +14,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  build: {
-    outDir: 'dist', // Imposta la directory di output
-  },
-})
+});
