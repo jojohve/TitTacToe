16576d9d6fba274828d4f9f687bea81d659c869c
jojohve <joseph.laluna@outlook.it>
2024-12-17 15:19:47 +0100

    dist

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,6 +14,11 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'app/dist',  // Aggiungi questa riga per configurare la cartella di destinazione
+    outDir: 'app/dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
   },
-});
+})
