66da294029f7693512445f1d7e0d5ab930a3ddcd
jojohve <joseph.laluna@outlook.it>
2025-01-17 16:19:24 +0100

    build

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './main.css';
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'structured-clone'
 
 createApp(App).mount('#app')
+
+function deepClone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      },
-    },
-  },
+      external: ['structured-clone']
+    }
+  }
 })
