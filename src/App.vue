b2cfd95cd836e65503746b713dbbbee8975b75d0
jojohve <joseph.laluna@outlook.it>
2025-01-17 15:42:32 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ const resetGame = () => {
         <div
           v-for="(cell, y) in row"
           :key="y"
-          @click="() => makeMove(x, y)"
+          @click="makeMove(x, y)"
           :class="['border', 'border-white', 'w-20', 'h-20', 'hover:bg-gray-700', 'flex', 'items-center', 'justify-center', 'material-icons-outlined', 'text-4xl', 'cursor-pointer', cell === 'X' ? 'text-pink-500' : 'text-blue-500']"
         >
           {{ cell === 'X' ? 'close' : cell === 'O' ? 'circle' : '' }}
