667568ffbe80d91d14a0feca811c135317134b2a
jojohve <joseph.laluna@outlook.it>
2025-01-17 16:29:55 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import './main.css';
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'structured-clone'
 
 createApp(App).mount('#app')
-
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
