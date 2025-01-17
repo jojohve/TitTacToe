bc989331cdd334584368d5b89a781194077aad77
jojohve <joseph.laluna@outlook.it>
2025-01-17 15:28:38 +0100

    module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./app/**/*.{vue,js,ts,jsx,tsx}"  // Aggiungi questa riga se i tuoi file sono nella directory app
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "./app/**/*.{vue,js,ts,jsx,tsx}" // Aggiungi questa riga se i tuoi file sono nella directory app
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
