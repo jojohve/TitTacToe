0e11ce19ef949a1137b7fd43dfcabb05581fbd2f
jojohve <joseph.laluna@outlook.it>
2025-01-17 15:51:10 +0100

    changes in node

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed } from 'vue';
 
-const player = ref('X')
+const player = ref('X');
 const board = ref([
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
-])
+]);
+
+const boardFlat = computed(() => board.value.flat());
 
 const calculateWinner = (squares) => {
   const lines = [
@@ -18,42 +20,37 @@ const calculateWinner = (squares) => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ]
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
+  ];
+  for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return squares[a];
     }
   }
-  return null
-}
+  return null;
+};
 
-const winner = computed(() => calculateWinner(board.value.flat()))
+const winner = computed(() => calculateWinner(boardFlat.value));
 
 const makeMove = (x, y) => {
-  if (winner.value || board.value[x][y] !== '') return
-
-  board.value[x][y] = player.value
-
-  player.value = player.value === 'X' ? 'O' : 'X'
-}
+  if (winner.value || board.value[x][y] !== '') return;
+  board.value[x][y] = player.value;
+  player.value = player.value === 'X' ? 'O' : 'X';
+};
 
 const resetGame = () => {
   board.value = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
-  ]
-  player.value = 'X'
-}
+  ];
+  player.value = 'X';
+};
 </script>
 
 <template>
   <main class="pt-8 text-center bg-gray-800 min-h-screen">
     <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe (Tris)</h1>
-
     <h3 class="text-xl mb-4">Turno di: {{ player }}</h3>
-
     <div class="flex flex-col items-center mb-8">
       <div v-for="(row, x) in board" :key="x" class="flex">
         <div
@@ -79,7 +76,6 @@ const resetGame = () => {
         </div>
       </div>
     </div>
-
     <h2 v-if="winner" class="text-6xl font-bold mb-8">Giocatore '{{ winner }}' vince!</h2>
     <button
       @click="resetGame"
